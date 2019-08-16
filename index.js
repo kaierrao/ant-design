7bfc8055c02fc9fc579f196e713134fd97744b40
kaierrao <1416180828@qq.com>
2019-08-16 09:37:53 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 /* eslint no-console:0 */
+
+驼峰命名转换
 function camelCase(name) {
   return name.charAt(0).toUpperCase() + name.slice(1).replace(/-(\w)/g, (m, n) => n.toUpperCase());
 }
 
 // Just import style for https://github.com/ant-design/ant-design/issues/3745
+// req（命名req?没搞懂）  引入默认样式 主题样式 base样式  解决全局样式冲突
 const req = require.context('./components', true, /^\.\/[^_][\w-]+\/style\/index\.tsx?$/);
 
 req.keys().forEach(mod => {
@@ -11,6 +14,8 @@ req.keys().forEach(mod => {
   if (v && v.default) {
     v = v.default;
   }
+  
+  // 组件转驼峰写法  除了message和notification
   const match = mod.match(/^\.\/([^_][\w-]+)\/index\.tsx?$/);
   if (match && match[1]) {
     if (match[1] === 'message' || match[1] === 'notification') {
